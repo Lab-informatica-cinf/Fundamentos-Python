3f5ab8e3eaadf2ea010f2035324e51b95ee3b77c
readingwritingcode <david.medina1@gmail.com>
2020-10-05 12:36:14 -0500

    string variables code. cheers programm :)

---
+++ cheersStringFormatting.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+''' 
+This script receive a person name, 
+and return "hello "person name", 
+welcome to labcinf2020"
+This code ask name twice. one for instructions
+other for the main function.
+'''
+
+#basic variable declaration,data input, data store, and  print instruction.
+person_name = input('what its your name?')
+
+cheers = 'hello %s, welcome to Labcinf2020' % person_name
+
+print(cheers)
+
+#create a function that encapsulate above instructions.
+
+def cheers_string_formatting():
+
+	'''function that encapsulate code. whit out 
+	   input params. output is print here. 
+	'''
+
+	person_name = input('what its your name?')
+
+	cheers = 'hello %s, welcome to Labcinf2020' % person_name
+
+	print(cheers)
+
+
+def main():
+	cheers_string_formatting()
+
+if __name__ == '__main__':
+	#main() for activate main function
+	print("enjoy it! :)")
